954be661831ba005d584adc94e3337418e96158b
jgkwak <jenniferkwak12@gmail.com>
2019-02-19 23:59:30 -0800

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -75,7 +75,7 @@ function isImportant(xhttp){
 
   for (i of array) {
     let new_p = document.createElement("p");
-    let label = document.createTextNode(array[i]);
+    let label = document.createTextNode(array[0]);
     new_p.appendChild(label);
     let first_child = para_node.firstChild;
     para_node.appendChild(new_p); 
