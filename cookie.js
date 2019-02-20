e29526b14ce203c20c5e24d4f7c34f7dca7c9180
jgkwak <jenniferkwak12@gmail.com>
2019-02-20 01:20:08 -0800

    Add files via upload

--- cookie.js
+++ cookie.js
@@ -68,7 +68,8 @@ function move() {
 
 function isImportant(xhttp){
   var para = xhttp.responseText;
-  var array = para.split("\n");
+  let newtext = xhttp.responseText.replace(/\\n/mg,"\n")
+  var array = newtext.split("\n");
   console.log(array[0]);
   var one = array[0];
   var word;
